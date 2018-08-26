58b115bfaa42057823c525f4299650aa8fc23b30
Joseph Bayer <josephbayer@Josephs-MacBook-Pro.local>
2018-08-26 15:32:12 -0500

    mtg api works now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as PIXI from 'pixi.js-legacy';
+import * as Magic from 'mtgsdk-ts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ProxyGenius';
+
+  ngOnInit() {
+    //let sprite = PIXI.Sprite.fromImage('assets/image.png');
+    //let magic = Magic.Cards.find('1').then(result => console.log(result.name));
+    Magic.Cards.find("08618f8d5ebdc0c4d381ad11f0563dfebb21f4ee").then(result => console.log(result.name)); // Blood Scrivener
+  }
+
 }
