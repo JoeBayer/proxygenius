5cf2e4b6542268a4d9083119dfaa9ec2e039eb16
Joseph Bayer <josephbayer@Josephs-MacBook-Pro.local>
2018-08-27 21:52:30 -0500

    Switched from magicthegather.io to scryfall api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //import * as PIXI from 'pixi.js-legacy';
-import * as Magic from 'mtgsdk-ts';
+import * as Scryfall from 'scryfall-sdk';
 
 @Component({
   selector: 'app-root',
@@ -9,22 +9,18 @@ import * as Magic from 'mtgsdk-ts';
 })
 export class AppComponent implements OnInit{
   title = 'ProxyGenius';
-  solRingSearchResults: Magic.Card[];
+  solRingSearchResults: Scryfall.Card;
 
   ngOnInit() {
-    //let magic = Magic.Cards.find('1').then(result => console.log(result.name));
-    let solRingSearchCriteria: Magic.CardFilter = {
-      name: 'Sol Ring'
-    }
 
-    Magic.Cards
-    .where(solRingSearchCriteria)
-    .then((searchResult: Magic.Card[])=> {
-      console.log('Search Results:');
-      this.solRingSearchResults = searchResult.filter((result)=>{
-        return result.imageUrl;
-      });
-      console.log(this.solRingSearchResults);
+    // Demo basic Scryfall search and display
+    Scryfall.Cards.search('sol-ring')
+    .on('data', card => {
+      this.solRingSearchResults = card;
+      console.log('received data for ' + card.name);
+    }).on('end', () => {
+      console.log('done');
     });
   }
+
 }
